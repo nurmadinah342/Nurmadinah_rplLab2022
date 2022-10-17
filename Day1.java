2177692972d43796a08e94726d405183270b0b1d
nurmadinah <m-0027199009@smkn2-maj.sch.id>
2022-10-17 21:24:06 +0700

    Tipe data

---
+++ Day1.java
@@ -0,0 +1,11 @@
+package tipedata;
+
+
+public class Day1 {
+   public static void main (String [] args){
+      //Deklarasi variabel
+      double pi = 3.14285714286;
+      //Output
+      System.out.println("Nilai pi = " + pi);
+}
+}
