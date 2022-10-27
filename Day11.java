9f6d434d5281615d4e9e01d93eb1fbd862bf4629
nurmadinah <m-0027199009@smkn2-maj.sch.id>
2022-10-27 22:11:28 +0700

    ArrayList

---
+++ Day11.java
@@ -0,0 +1,11 @@
+
+public class Day11 {
+
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars[0]);
+        System.out.println(cars[1]);
+        System.out.println(cars[2]);
+        System.out.println(cars[3]);
+    }
+}
