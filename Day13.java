4b1cc7491af8fe04002303533f262dab9abb0bb8
nurmadinah <m-0027199009@smkn2-maj.sch.id>
2022-10-29 20:12:19 +0700

    nilai rata-rata

---
+++ Day13.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Day13 {
+
+    public static void main(String args[]) {
+        Scanner masuk = new Scanner(System.in);
+        System.out.println("MENGHITUNG NILAI RATA-RATA");
+        int i, n;
+        float jum, x, rata;
+        System.out.print("Banyaknya Data: ");
+        n = masuk.nextInt();
+        jum = 0;
+        i = 1;
+        while (i <= n) {
+            System.out.print("Data ke-" + i + ": ");
+            x = masuk.nextFloat();
+            jum += x;
+            i++;
+        }
+        rata = jum / n;
+        System.out.println("Rata-rata: " + rata);
+        System.out.println("Jumlah: " + jum);
+    }
+}
