86b3bd8402ee4d1017f303b9b6a9ae386e348dee
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2022-10-31 19:59:42 +0800

    enum

---
+++ day14.java
@@ -0,0 +1,14 @@
+
+package operatorr;
+enum Level {
+  Rendah,
+  Sedang,
+  Tinggi
+}
+
+public class day14{ 
+  public static void main(String[] args) { 
+    Level myVar = Level.Tinggi; 
+    System.out.println(myVar); 
+  } 
+}
