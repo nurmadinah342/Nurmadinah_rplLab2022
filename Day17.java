d4035a97345af5b2e4bb31bcf2dcfc199cf51859
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2022-11-02 20:18:27 +0800

    continue

---
+++ Day17.java
@@ -0,0 +1,13 @@
+
+public class Day17 {
+
+    public static void main(String[] args) {
+        for (int angka = 0; angka < 8; angka++) {
+            if (angka == 5) {
+                continue;
+            }
+            System.out.println("angka ke-" + angka);
+        }
+    }
+
+}
