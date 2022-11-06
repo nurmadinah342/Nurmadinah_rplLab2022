75b59aeb50c0ba50296b513f8a045f838b9f6fb1
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2022-11-06 21:15:44 +0700

    float

---
+++ Day21.java
@@ -0,0 +1,8 @@
+
+public class Day21 {
+    public static void main(String[] args) {
+        float nilai = 5.99f;    // floating point number
+        System.out.println(nilai);
+    }
+        
+}
