6faa81b31a72af5802f1f26fcfa021af3fef07d1
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2022-11-07 21:48:40 +0700

    abstract

---
+++ Day22.java
@@ -0,0 +1,12 @@
+
+public class Day22 {
+
+    abstract class Animal {
+
+        public abstract void animalSound();
+
+        public void sleep() {
+            System.out.println("Zzz");
+        }
+    }
+}
