1277b3a9c5314633603a08f2545a384a6e3a2976
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2022-11-11 21:51:50 +0700

    Menggunakan Method

---
+++ Day26.java
@@ -0,0 +1,13 @@
+
+public class Day26 {
+static void myMethod() {
+System.out.println("I just got executed!");
+}
+
+
+public static void main(String[] args) {
+myMethod();
+myMethod();
+myMethod();
+}
+}
