18d0fbe13e9242cc9c09470d872d18503ec21d1f
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2022-11-13 21:05:58 +0700

    menghitung nilai rata-rata

---
+++ day28.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipeData;
+
+/**
+ *
+ * @author Lenovo T460s Touch
+ */
+public class day28 {
+    public static void main(String[] args) {
+        int a=5;
+        int b=2;
+        int c=8;
+        int d=5;
+        int e=4;
+        int jumdata=5; 
+        double rata_rata=a+b+c+d+e/jumdata;
+        System.out.print("Rata-rata:" +rata_rata);   
+    }   
+}
