a9205352d7ac04e114782710633cfc08910e6365
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2022-11-14 22:04:04 +0700

    Logika OR

---
+++ Day29.java
@@ -0,0 +1,16 @@
+
+public class Day29 {
+    public static void main(String[] args) {
+        boolean hasil;
+
+        hasil = (false && true) || (true || false);
+        System.out.println("Hasil: " + hasil);
+
+        hasil = !false && (false || true);
+        System.out.println("Hasil: " + hasil);
+
+        hasil = ((true && true) || (true || false)) && !true;
+        System.out.println("Hasil: " + hasil);
+    }
+    
+}
