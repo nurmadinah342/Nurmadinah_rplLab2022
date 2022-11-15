a94478486d9ead491cdbcf05e2282a9674088178
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2022-11-15 21:41:17 +0700

    Perulangan For Bersarang

--- Day30.java
+++ Day30.java
@@ -4,22 +4,11 @@ import java.util.Scanner;
 
 public class Day30 {
 public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("Masukkan nilai A : ");
-        int a = input.nextInt();
-        
-        System.out.print("Masukkan nilai B : ");
-        int b = input.nextInt();
-        
-        if (a > b){
-            int simpan = a;
-            a = b;
-            b = simpan;
-        }
-        System.out.println("NIlai a : " + a);
-        System.out.println("NIlai b : " + b);
-        
-        
-    }
+        int bilangan1;
+        int bilangan2;
+        for (bilangan1=1; bilangan1<=4; bilangan1++) {
+            for (bilangan2=1; bilangan2<=2; bilangan2++) {
+                System.out.println("[" + bilangan1 + "]" + "[" + bilangan2 + "]");
+                
+            }
 }
