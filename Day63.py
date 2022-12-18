3ab4a488156f9b797d5903b52910fb20b2884529
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2022-12-18 22:04:37 +0800

    menggabung dictionary

---
+++ Day63.py
@@ -0,0 +1,20 @@
+identitas = { 
+    "nama" : "madinah", 
+    "umur" : 19, 
+    "alamat" : "majene"} 
+identitas2 = { "jurusan" : "Informatika",
+ "kelas" : "C", 
+ "hobbi" : "membaca"}
+
+
+print("identitas : ", identitas) 
+print() 
+print("identitas2 : ", identitas2) 
+print()
+hasil = dict(identitas, **identitas2) 
+print("setelah di gabung : ", hasil) 
+print() 
+
+print(hasil.keys()) 
+print() 
+print(hasil.values())
