0abd2436564c4766e67b1db3df59dde8b53ebfa0
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-01-12 21:16:51 +0700

    Menghitug keuntungan Investasi

---
+++ Day88.py
@@ -0,0 +1,14 @@
+print("===== ISIAN INVESTASI ======")
+print()
+awal = int(input("Masukkan Modal Awal Investasi : "))
+lama = int(input("Masukkan Lama Investasi : "))
+
+total = awal * lama
+keuntungan = (total * 5)/100
+
+print()
+print("==== RINCIAN INVESTASI ====")
+print("Modal Awal       : ", awal)
+print("Lama Investasi   : ", lama)
+print("Total Investasi  : ", total)
+print("Keuntungan       : ", keuntungan)
